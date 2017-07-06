2c70db7a1b04831ea44438bb9d3dfd8ee0019bfe
Allugard <cen4ukk@gmail.com>
2017-07-06 20:00:30 +0300

    connectionManager with ThreadLocal

--- src/main/java/ua/allugard/hotel/model/dao/util/ConnectionManager.java
+++ src/main/java/ua/allugard/hotel/model/dao/util/ConnectionManager.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
 
     private DataSource dataSource;
+    private static ThreadLocal<DatabaseConnection> connectionThreadLocal = new ThreadLocal<>();
 
     private ConnectionManager() {
         try {
@@ -30,10 +31,37 @@ public class ConnectionManager {
     }
 
     public synchronized DatabaseConnection getConnection() {
-        try {
-            return new JdbcConnection(dataSource.getConnection());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if(connectionThreadLocal.get() == null) {
+            try {
+                return new JdbcConnection(dataSource.getConnection());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return connectionThreadLocal.get();
         }
     }
+
+    public void startTransaction(){
+        DatabaseConnection connection = getConnection();
+        connection.startTransaction();
+        connectionThreadLocal.set(connection);
+    }
+    public void commit(){
+        DatabaseConnection connection = connectionThreadLocal.get();
+        connection.commit();
+        close(connection);
+    }
+
+    public void rollback(){
+        DatabaseConnection connection = connectionThreadLocal.get();
+        connection.rollback();
+        close(connection);
+    }
+
+    private void close(DatabaseConnection connection){
+        connectionThreadLocal.remove();
+        connection.close();
+    }
+
 }
